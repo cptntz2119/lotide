9ecd84076b86d782f9cbf3f9259c05df9470c387
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-09 14:00:07 -0800

    final version of eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -10,14 +10,15 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        return true;
-      } else {
-        return false;
-      }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
   }
+  return true;
 };
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
