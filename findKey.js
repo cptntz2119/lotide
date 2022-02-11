b314e6d8867732f8ad2ca3e309273de68f7d0c9f
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-10 16:50:50 -0800

    findKey using callback

---
+++ findKey.js
@@ -0,0 +1,52 @@
+const assertEqual = function (actual, expected) {
+  const emojiFail = String.fromCodePoint(0x1f6d1);
+  const emojiPass = String.fromCodePoint(0x1f7e9);
+  if (eqArrays(actual, expected)) {
+    console.log(
+      `${emojiPass}${emojiPass}${emojiPass} Assertion Passed: ${actual} === ${expected}`
+    );
+  } else {
+    console.log(
+      `${emojiFail}${emojiFail}${emojiFail} Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const findKey = (obj, callback) => {
+  const result = [];
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      result.push(key);
+      break;
+    }
+  }
+  //console.log(Array.isArray(result));
+  return result;
+};
+
+const result1 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+assertEqual(result1, ["noma"]);
