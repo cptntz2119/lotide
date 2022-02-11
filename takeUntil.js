8fab551f6d191bf04d73320dcaefc676fb4b2745
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-10 16:15:57 -0800

    final version with assertEqual

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,30 @@
+const assertEqual = function (actual, expected) {
+  const emojiFail = String.fromCodePoint(0x1f6d1);
+  const emojiPass = String.fromCodePoint(0x1f7e9);
+  if (eqArrays(actual, expected)) {
+    console.log(
+      `${emojiPass}${emojiPass}${emojiPass} Assertion Passed: ${actual} === ${expected}`
+    );
+  } else {
+    console.log(
+      `${emojiFail}${emojiFail}${emojiFail} Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const takeUntil = function (array, callback) {
   // ...
   let result = [];
@@ -30,3 +57,5 @@ const data2 = [
 ];
 const results2 = takeUntil(data2, (x) => x === ",");
 console.log(results2);
+assertEqual(results1, [1, 2, 5, 7, 2]);
+assertEqual(results2, ["I've", "been", "to", "Hollywood"]);
