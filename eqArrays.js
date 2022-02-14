4cf03965cca6138dca9c594bae456573c163a21d
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-14 10:01:44 -0800

    use module to refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,4 @@
-const assertEqual = function (actual, expected) {
-  const emojiFail = String.fromCodePoint(0x1f6d1);
-  const emojiPass = String.fromCodePoint(0x1f7e9);
-  if (actual === expected) {
-    console.log(
-      `${emojiPass}${emojiPass}${emojiPass} Assertion Passed: ${actual} === ${expected}`
-    );
-  } else {
-    console.log(
-      `${emojiFail}${emojiFail}${emojiFail} Assertion Failed: ${actual} !== ${expected}`
-    );
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
@@ -24,4 +12,5 @@ const eqArrays = (arr1, arr2) => {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+module.exports = eqArrays;
