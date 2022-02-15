aa6dfbe70f715ff0dd50ff474cc864e666db85c6
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-14 20:31:16 -0800

    refactor via chai

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,18 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  const emojiFail = String.fromCodePoint(0x1f6d1);
-  const emojiPass = String.fromCodePoint(0x1f7e9);
-  if (actual === expected) {
-    console.log(
-      `${emojiPass}${emojiPass}${emojiPass} Assertion Passed: ${actual} === ${expected}`
-    );
-  } else {
-    console.log(
-      `${emojiFail}${emojiFail}${emojiFail} Assertion Failed: ${actual} !== ${expected}`
-    );
-  }
-};
-
+const assertEqual = require("./assertEqual");
 const findKeyByValue = (objects, value) => {
   for (let key in objects) {
     if (objects[key] === value) {
@@ -22,11 +9,11 @@ const findKeyByValue = (objects, value) => {
   return undefined;
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama: "The Wire",
+// };
+module.exports = findKeyByValue;
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
