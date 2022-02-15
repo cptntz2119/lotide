dc2fe06892d615ec67befca5aacbe9f7cbff24fd
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-14 20:18:26 -0800

    refactor via chai

--- letterPositions.js
+++ letterPositions.js
@@ -1,4 +1,3 @@
-
 const letterPositions = function (sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
@@ -14,35 +13,35 @@ const letterPositions = function (sentence) {
 
   return results;
 };
-const assertEqual = function (actual, expected) {
-  const emojiFail = String.fromCodePoint(0x1f6d1);
-  const emojiPass = String.fromCodePoint(0x1f7e9);
-  if (eqArrays(actual, expected)) {
-    console.log(
-      `${emojiPass}${emojiPass}${emojiPass} Assertion Passed: ${actual} === ${expected}`
-    );
-  } else {
-    console.log(
-      `${emojiFail}${emojiFail}${emojiFail} Assertion Failed: ${actual} !== ${expected}`
-    );
-  }
-};
+// const assertEqual = function (actual, expected) {
+//   const emojiFail = String.fromCodePoint(0x1f6d1);
+//   const emojiPass = String.fromCodePoint(0x1f7e9);
+//   if (eqArrays(actual, expected)) {
+//     console.log(
+//       `${emojiPass}${emojiPass}${emojiPass} Assertion Passed: ${actual} === ${expected}`
+//     );
+//   } else {
+//     console.log(
+//       `${emojiFail}${emojiFail}${emojiFail} Assertion Failed: ${actual} !== ${expected}`
+//     );
+//   }
+// };
+const assertEqual = require("./assertEqual");
+// const eqArrays = (arr1, arr2) => {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
 
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+const eqArrays = require("./eqArrays");
 //console.log(letterPositions("lighthouse in the house"));
-console.log(letterPositions("hello"));
-assertEqual(letterPositions("hello"), { h: [0], e: [1], l: [2, 3], o: [4] });
-assertEqual(letterPositions("hello").e, [1]);
-
+// console.log(letterPositions("hello"));
+// assertEqual(letterPositions("hello"), { h: [0], e: [1], l: [2, 3], o: [4] });
+// assertEqual(letterPositions("hello").e, [1]);
+module.exports = letterPositions;
