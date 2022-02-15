d72831afc1e8ef163d8ffaec0b3dff98232ae124
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-14 20:40:32 -0800

    test file for eqObject

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,22 @@
+const assert = require("chai").assert;
+const eqObjects = require("../eqObjects");
+
+describe("#eqObjects", () => {
+  it("should returns 'true' for 'eqObects(cd, dc)'", () => {
+    const cd = { c: "1", d: ["2", 3] };
+    const dc = { d: ["2", 3], c: "1" };
+    assert.strictEqual(eqObjects(cd, dc), true);
+  });
+  it("should returns 'false' for 'eqObects(cd, cd2)'", () => {
+    const cd = { c: "1", d: ["2", 3] };
+    const cd2 = { c: "1", d: ["2", 3, 4] };
+    assert.strictEqual(eqObjects(cd, cd2), false);
+  });
+});
+
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc)); // => true
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2)); // => false
